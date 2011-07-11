1611029ed3378eb505b8ca7ad32e9eac55deb935
Konboi <dv.vb.dv.vb.dv.vb.dv.vb@gmail.com>
2011-07-11 18:06:15 +0900

    サイズ指定可能に

--- resize.pl
+++ resize.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+
 use Imager;
 use FindBin;
 use File::Path;
@@ -11,31 +12,31 @@ my $dir = $FindBin::Bin;
 my $save_dir = dir( $dir, 'small' );
 
 my $img = Imager->new;
-my $standard_length = 800; # width or height 
 
-#1.画像を読み込む
-my @file = glob "*.JPG *.jpg";
+print ( '変換する画像の縦を指定してください:');
+my $standard_length = <STDIN> ; # width or height
+
+#画像ファイルを読み込む
+my @file = glob "*.JPG *.jpg *jpeg *.png *.PNG *.gif";
 my $file_num = @file;
 
 if( -d $save_dir ){
 }
 
 else{
-    print( 'can  not find \n' );
     print( $save_dir );
-    mkpath( "small" ) or die $!;
+    mkpath( "$dir/small" ) or die $!;
 }
 
 
 for( my $i = 0; $i < $file_num ; $i++){
     print(" $i :: @file[$i] \n ");
+    #my $file_type = @file[i] =~ /^.+\.(.+?)$/ ? $1 : 'gif';
+
     $img->read( file => "$dir/@file[$i]");
 
-    my $pict_width = $img->getwidth;
-    my $pict_height = $img->getheight;
-
-    my $stand = $standard_length / $pict_width;
-    my $stand_height = $pict_height * $stand;
+    my $stand = $standard_length / $img->getwidth;
+    my $stand_height = $img->getheight * $stand;
 
     $img = $img->scale(
     xpixels => $standard_length,
@@ -43,5 +44,5 @@ for( my $i = 0; $i < $file_num ; $i++){
     );
 
     my $save_file = file( $dir, "small", @file[$i] );
-    $img->write( file => $save_file, jpegquality => 90 )
+    $img->write( file => $save_file, jpegquality => 100 )
 }
